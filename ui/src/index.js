5f3c3fc0058a77c0e802b110d507c84eb19c49ad
Kelvin Ho <kelvin.ho.ka.wai@gmail.com>
2018-01-13 17:31:16 +0800

    Create redux middleware

--- ui/src/index.js
+++ ui/src/index.js
@@ -1,8 +1,29 @@
+import 'babel-polyfill';
+
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {Provider} from 'react-redux';
+import {BrowserRouter, Route} from 'react-router-dom'
+import {createStore, applyMiddleware} from 'redux';
+import thunk from 'redux-thunk';
+import promise from 'redux-promise';
+import {createLogger} from 'redux-logger';
+import 'bootstrap/dist/css/bootstrap.css';
 import './index.css';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import appReducer from './reducers';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+
+const logger = createLogger();
+const store = createStore(
+    appReducer,
+    applyMiddleware(thunk, promise, logger)
+);
+
+
+ReactDOM.render(
+<Provider store={store}>
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+</Provider>, document.getElementById('root'));
