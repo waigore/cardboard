1a85c55d5e0bff842388001ee2618b22a7c4a1f0
Kelvin Ho <32946806+waigore@users.noreply.github.com>
2018-02-06 09:35:58 +0800

    add script to insert testing data into DB

---
+++ api/src/tools/create-test-data.js
@@ -0,0 +1,62 @@
+const fs = require('fs-extra');
+const moment = require('moment');
+
+const Image = require('../sequelize/models').Image;
+
+const util = require('../util');
+
+let images = [
+  {
+    identifier: 10000,
+    filename: 'link.jpg',
+    tags: 'zelda',
+    characters: 'link',
+    copyrights: 'zelda',
+    uploadedAt: moment().subtract(4, 'days')
+  },
+  {
+    identifier: 10001,
+    filename: 'link02.jpg',
+    tags: 'zelda',
+    characters: 'link',
+    copyrights: 'zelda',
+    uploadedAt: moment().subtract(3, 'days')
+  },
+  {
+    identifier: 10002,
+    filename: 'link03.jpg',
+    tags: 'zelda',
+    characters: 'link',
+    copyrights: 'zelda',
+    uploadedAt: moment().subtract(2, 'days')
+  },
+  {
+    identifier: 10003,
+    filename: 'link04.jpg',
+    tags: 'zelda',
+    characters: 'link',
+    copyrights: 'zelda',
+    uploadedAt: moment().subtract(1, 'days')
+  },
+  {
+    identifier: 10004,
+    filename: 'link05.jpg',
+    tags: 'zelda',
+    characters: 'link',
+    copyrights: 'zelda',
+    uploadedAt: moment()
+  }
+]
+
+Promise.resolve(images)
+.then(images => {
+  return images.map(image => {
+    image.fileUrl = '/data/' + image.filename;
+    image.site = 'danbooru';
+    return image;
+  })
+})
+.then(images => {
+  return Image.bulkCreate(images)
+})
+.then(() => console.log('Done!'))
